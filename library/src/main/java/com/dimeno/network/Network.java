a0be169f7fccdd5ff46aac51ff8270fcac4c017a
王震 <lavalike@yeah.net>
2020-04-24 08:57:06 +0800

    扩展初始化方法

--- library/src/main/java/com/dimeno/network/Network.java
+++ library/src/main/java/com/dimeno/network/Network.java
@@ -1,5 +1,7 @@
 package com.dimeno.network;
 
+import android.content.Context;
+
 import com.dimeno.network.config.NetConfig;
 
 /**
@@ -7,9 +9,17 @@ import com.dimeno.network.config.NetConfig;
  * Created by wangzhen on 2020/4/15.
  */
 public final class Network {
+    public static Context sContext;
     public static NetConfig sConfig;
 
     public static void init(NetConfig config) {
+        init(null, config);
+    }
+
+    public static void init(Context context, NetConfig config) {
+        if (sContext != null) {
+            sContext = context.getApplicationContext();
+        }
         sConfig = config;
     }
 }
